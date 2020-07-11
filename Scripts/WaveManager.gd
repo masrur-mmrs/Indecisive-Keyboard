1efd1b67ffae42f1c2e09b2ac46c0f48d6a296fb
SadmanTariq <sadmantariq@gmail.com>
2020-07-11 22:31:15 +0600

    Add wave spawning

---
+++ Scripts/WaveManager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+export var time_between_waves = 3
+export var autostart = true
+
+var current_wave = -1
+
+func _ready():
+	set_process(false)
+	if autostart:
+		set_process(true)
+		start()
+		
+func _process(_delta):
+	if get_tree().get_nodes_in_group("enemy").size() <= 0:
+		if $InterwaveTimer.time_left <= 0:
+			$InterwaveTimer.start(time_between_waves)
+
+func start():
+	current_wave += 1
+	_next_wave()
+
+func _next_wave():
+	for spawner in get_tree().get_nodes_in_group("spawner"):
+		spawner.start_wave(current_wave + 1)
