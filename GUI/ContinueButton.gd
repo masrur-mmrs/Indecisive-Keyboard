63d4987122898125c66d18504e01ccbcafd15760
SadmanTariq <sadmantariq@gmail.com>
2020-07-12 14:03:27 +0600

    Delete some unnecessary files

--- GUI/ContinueButton.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-onready var scene = get_node("SceneChanger")
-
-func _on_ContinueButton_pressed():
-	scene.change_scene("res://MainScenes//Main.tscn")#yo sadman change this to the last checkpoint alright?
